08a78697a49290e1bdb9f3d154596e4f324a2fad
Caden Cardell <cadencardell@gmail.com>
2026-06-08 17:48:32 -0400

    second thing works

--- experiments/fdk-filter-new/fdk_filter_new_test.sh
+++ experiments/fdk-filter-new/fdk_filter_new_test.sh
@@ -10,22 +10,20 @@ module load conda
 
 conda activate mbirjax
 
-#for ((views=512; views<65536; views+=512)); do
-#  echo ""
-#  echo "Filtering sinogram with shape ($views, 512, 512)"
-#  python fdk_filter.py "$views" 512 512 || break
-#done
-
-#for ((rows=21504+512; rows<65536; rows+=512)); do
-#  echo ""
-#  echo "Filtering sinogram with shape (512, $rows, 512)"
-#  python fdk_filter.py 512 "$rows" 512 || break
-#done
-
-# 512,512,8192
-
-for ((channels=7680+512; channels<65536; channels+=512)); do
+for ((views=512; views<65536; views+=512)); do
   echo ""
-  echo "Filtering sinogram with shape (512, 512, $channels)"
-  python fdk_filter.py 512 512 "$channels" || break
+  echo "Filtering sinogram with new method with shape ($views, 512, 512)"
+  python fdk_filter_new.py "$views" 512 512 || break
+done
+
+for ((rows=512; rows<65536; rows+=512)); do
+  echo ""
+  echo "Filtering sinogram with new method with shape (512, $rows, 512)"
+  python fdk_filter_new.py 512 "$rows" 512 || break
+done
+
+for ((channels=512; channels<65536; channels+=512)); do
+  echo ""
+  echo "Filtering sinogram with new method with shape (512, 512, $channels)"
+  python fdk_filter_new.py 512 512 "$channels" || break
 done
